1069c1e6f35d1bc34f1197d2e749a111d47238ac
Kaan <kaanc6286@gmail.com>
2024-02-10 20:40:32 +0300

    Kendi Not Defterim
    Windows Form ile tasarladığım ilk uygulama

---
+++ Kendi Not Defterim/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Txt seçiniz | *.txt";
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Txt seçiniz | *.txt";
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text);
+            MessageBox.Show("Kaydedildi");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog2.Filter = "Txt seçiniz | *.txt";
+            saveFileDialog2.ShowDialog();
+        }
+
+        private void saveFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            File.AppendAllText(saveFileDialog2.FileName, richTextBox1.Text);
+            MessageBox.Show("Eklendi");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            File.Delete(openFileDialog2.FileName);
+            MessageBox.Show("Silindi");
+        }
+    }
+}
